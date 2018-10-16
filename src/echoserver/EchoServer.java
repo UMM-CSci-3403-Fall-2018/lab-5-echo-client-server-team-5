ee9e57a2b9dd79fb5f01e32cd6e1225f602ddfb6
rabex028 <rabex028@morris.umn.edu>
2018-10-16 11:22:26 -0500

    Start client and server files
    Client and server files have been created
    with input and output streams that are
    attempting to read individual bits being
    typed into the keyboard to be sent over
    the network.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,30 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
 public class EchoServer {
+  public static final int portnumber = 6013;
+
+  public static void main(String[] args) throws IOException{
+    try{
+      // Start listening on the specified port
+      ServerSocket sock = new ServerSocket(portnumber);
+
+      while (true) {
+      Socket client = sock.accept();
+      System.out.println("Got a request!");
+
+      PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
+
+      writer.println();
+
+      // Close the client socket since we're done.
+      client.close();
+    }
+  } catch (IOException ioe) {
+    System.out.println("We caught an unexpected exception");
+    System.err.println(ioe);
+  }
+ }
 }
